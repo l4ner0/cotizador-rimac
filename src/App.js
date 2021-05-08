28c40f0caaa1bede894acb1d289f11bd7e36bfb4
Diego <diego.urbina@acceso.com.pe>
2021-05-08 12:33:42 -0500

    Se depuró archivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import SwitchGroup from './SwitchGroup'
 
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="secondary">Hello World</Button>
-      <SwitchGroup label="COtización"/>
+     Hola Mundo
     </div>
   );
 }
