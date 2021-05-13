855948559872913cabd4eb0cb58b93a01f695a1f
Diego <diego.urbina@acceso.com.pe>
2021-05-13 14:31:53 -0500

    Se modificó tipo de enrutamiento

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+    </HashRouter>
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
