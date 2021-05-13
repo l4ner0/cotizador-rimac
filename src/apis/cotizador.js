0530a9322a54f392b368972aa17a62178a4cf153
Diego <diego.urbina@acceso.com.pe>
2021-05-13 18:31:27 -0500

    Se modificó la base de la URL

--- src/apis/cotizador.js
+++ src/apis/cotizador.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://my-json-server.typicode.com/l4ner0/api-cotizador';
+const BASE_URL = 'https://my-json-server.typicode.com/l4ner0/api-cotizador';
 
 export const apiQuoteInsurance = (payload) => axios.get(`${BASE_URL}/quoteInsurance`, payload);
 export const apiVehicleYears = () => axios.get(`${BASE_URL}/vehicleYears`);
