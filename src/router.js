1a0dbd2fbff682ba4d27a8d9b9bc4bb23f1ddee2
Diego <diego.urbina@acceso.com.pe>
2021-05-16 14:19:21 -0500

    Se culminó el diseño de la pantalla final

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './components/login/Login';
 import MisDatos from './components/mis-datos/MisDatos';
 import ArmaTuPlan from './components/arma-tu-plan/ArmaTuPlan';
+import ConfirmacionCompra from './components/confirmacion-compra/ConfirmacionCompra';
 
 export const LOGIN = '/cotizador-rimac';
 export const MIS_DATOS = '/cotizador-rimac/mis-datos';
 export const ARMA_TU_PLAN = '/cotizador-rimac/arma-tu-plan';
+export const CONFIRMACION_COMPRA = '/cotizador-rimac/confirmacion_compra';
 
 const Routes = () => {
     return (
@@ -14,6 +16,7 @@ const Routes = () => {
             <Route exact path={LOGIN} component={Login} />
             <Route exact path={MIS_DATOS} component={MisDatos} />
             <Route exact path={ARMA_TU_PLAN} component={ArmaTuPlan} />
+            <Route exact path={CONFIRMACION_COMPRA} component={ConfirmacionCompra}/>
         </Switch>
     )
 }
